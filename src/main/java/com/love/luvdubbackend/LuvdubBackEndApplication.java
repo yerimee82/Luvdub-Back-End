5b2137a3b2cc66daec58f07084a69554298eb02d
Kim, Yerim <yerim343@naver.com>
2024-01-23 20:11:35 +0900

    initial commit

---
+++ src/main/java/com/love/luvdubbackend/LuvdubBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.love.luvdubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuvdubBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuvdubBackEndApplication.class, args);
+    }
+
+}
