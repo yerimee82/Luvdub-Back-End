63ae1e9babc5096475567bb60619469483938c84
Kim, Yerim <yerim343@naver.com>
2024-01-23 20:11:35 +0900

    initialcommit

---
+++ src/test/java/com/love/luvdubbackend/LuvdubBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.love.luvdubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuvdubBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
